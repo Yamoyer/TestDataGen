6216b7c912d5f6b693bb384a382591a155aa4e2e
Yamoyer <48922307+Yamoyer@users.noreply.github.com>
2019-04-04 19:39:18 -0500

    Add files via upload
    Can no longer resize window, sizing of buttons is different, now sports a delete row button for the specific row.

--- refactored2.py
+++ refactored2.py
@@ -8,8 +8,6 @@ import os
 import random
 import openpyxl
 
-myDB = pydbgen.pydb()
-
 
 class TestDataGen(Frame):
     newMenuList = ['ssn', 'name','country', 'date', 'company','state','city', 'real_(US)_cities',
@@ -19,7 +17,7 @@ class TestDataGen(Frame):
     entryList = []
     buttonListVals = []    
     entryListVals =[]
-
+    myDB = pydbgen.pydb()
     def __init__(self, master=None):
         Frame.__init__(self,master)
         self.master = master
@@ -28,64 +26,93 @@ class TestDataGen(Frame):
     def init_window(self):
         self.master.title("Test Data Generator")
         self.master.config(background = 'light blue')
-        self.master.geometry('450x400+0+0')
+        self.master.geometry('400x400+0+0')
 
-        lbl1 = Label(self.master, text='Field Types') #basic grid label
-        lbl1.grid(column=2, row=2, pady=10, padx = 20)
-        lbl1.config(font=("Hevetica", 15), background = 'light blue')
+        self.master.lbl1 = Label(self.master, text='Field Types') #basic grid label
+        self.master.lbl1.grid(column=2, row=2, pady=10, padx = 5)
+        self.master.lbl1.config(font=("Hevetica", 15), background = 'light blue')
 
-        lbl = Label(self.master, text='Field Names') #basic grid label
-        lbl.grid(column=0, row=2)
-        lbl.grid_columnconfigure((0,1,2), weight = 1)
-        lbl.config(font=("helvetica", 15), background = 'light blue')
+        self.master.lbl = Label(self.master, text='Field Names') #basic grid label
+        self.master.lbl.grid(column=0, row=2, padx = 20)
+        self.master.lbl.grid_columnconfigure((0,1,2), weight = 1)
+        self.master.lbl.config(font=("helvetica", 15), background = 'light blue')
 
-        entRows = Entry(self.master, width = 13)
-        entRows.grid (column = 1, row = 1)
-        entRows.focus_set()
+        self.master.entRows = Entry(self.master, width = 8)
+        self.master.entRows.grid (column = 1, row = 1)
+        self.master.entRows.focus_set()
 
-        lblRows = Label(self.master, text = 'Number of rows:')
-        lblRows.grid(column = 0, row=1,  pady=5, padx = 20)
-        lblRows.config(font=("Helvetica", 15), background = 'light blue')
+        self.master.lblRows = Label(self.master, text = 'Number of rows:')
+        self.master.lblRows.grid(column = 0, row=1,  pady=5, padx = 5)
+        self.master.lblRows.config(font=("Helvetica", 15), background = 'light blue')
 
-
-        addRowButton = Button(self.master, text = 'Add Row', command = self.generateData)
-        addRowButton.grid(column = 2, row = 8)        
+        self.master.addRowButton = Button(self.master, text = 'Add Row', command = self.addEntryandButton)
+        self.master.addRowButton.grid(column = 2, row = 8)        
+        self.master.addRowButton.configure(takefocus = 0)
         
+        self.master.destroyRow = Button(self.master, text = 'X', command = self.destroyButtonRow )
+        self.master.destroyRow.config(width = 2)
+        self.master.destroyRow.grid(column=3, row = 3 )
+
         self.master.entry1 = Entry(self.master, width = 13)
         self.master.entry1.grid(column = 0, row = 3, pady = 5)
+        self.master.entry1.configure(takefocus = 1)
 
-        self.master.entry2 = Entry(self.master, width = 13)
-        self.master.entry2.grid(column = 0, row = 4, pady = 5)
+        self.master.button1 = Button(self.master, text = TestDataGen.newMenuList[0], command = self.buttonChange)
+        self.master.button1.grid(column = 2, row = 3, pady = 5)
+        self.master.button1.configure(takefocus = 0) 
 
-        button1 = Button(self.master, text = '', command = self.buttonChange)
-        button1.grid(column = 2, row = 3, pady = 5)
-
-        button2 = Button(self.master, text = '', command = self.buttonChange)
-        button2.grid(column = 2, row = 4, pady = 5)
-          
-
-        dataBtn = Button(self.master, text='Generate Data', command= self.entryListAppend)
+        dataBtn = Button(self.master, text='Generate Data', command = self.entryAndButtonListAppend)
         dataBtn.grid(column = 1, row = 8, pady = 5, padx = 5, sticky = NSEW) 
         dataBtn.configure(takefocus = 0) 
-    
- 
     
     def buttonChange(self):
         buttonMenu = Tk()
         buttonMenu.title('Button Menu')
-
         buttonMenu.mainloop()
 
-    def entryListAppend(self):
+    def entryAndButtonListAppend(self):
         TestDataGen.entryList.append(self.master.entry1.get())
-        TestDataGen.entryList.append(self.master.entry2.get())
-        
+        TestDataGen.buttonList.append(self.master.button1.cget('text'))
+
+        print(self.master.entRows.get())
+        print(TestDataGen.buttonList) 
         print(TestDataGen.entryList)
+       
+        TestDataGen.entryList.clear()
+        TestDataGen.buttonList.clear()
+
+    def destroyButtonRow(self):
+        self.master.entry1.destroy()
+        self.master.button1.destroy()
+        self.master.destroyRow.destroy()
 
     def generateData(self):
         generateWindow = Tk()
 
-           
+    def addEntryandButton(self):
+
+        self.buttonVar = Button(self.master, text = '')
+        self.buttonVar.grid(column = 2, row = 4 )
+        TestDataGen.buttonList.append(self.buttonVar.cget('text'))
+
+        print(TestDataGen.buttonList)
+        print(self.buttonVar)
+
+# def generateData():
+#container class for buttons    
+# class buttons():
+#     def __init__(self, buttonListAct):
+#         self.buttonListAct = []
+#         self.button =  buttons
+#     def addButton(self, button):
+#         self.buttonListAct.append(button)
+
+#button class
+# class button():
+#     Button(TestDataGen, text = TestDataGen.newMenuList, command = buttons.addButton)
+
+
 root = Tk()
 gui = TestDataGen(root)
+root.resizable(0, 0)
 root.mainloop()
